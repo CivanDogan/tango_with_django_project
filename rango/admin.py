ab43b99ce05a07a47aa9e02c2dc4389633c58b9f
Civan Dogan <ciwoerim@gmail.com>
2022-02-09 17:50:10 +0000

    Test9 passed

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
+
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -12,5 +13,6 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
 
